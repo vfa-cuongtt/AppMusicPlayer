e6f4a45ab9c7aea85af9ac9100298460b88fdff4
CuongTT <vfa.cuongtt@gmail.com>
2016-11-09 17:54:56 +0700

    TesHomeView OK

--- components/layouts/TestHomeView.js
+++ components/layouts/TestHomeView.js
@@ -28,10 +28,12 @@ var RNFS = require('react-native-fs');
 import MediaMeta from 'react-native-media-meta';
 var DATA = new Array();
 var MetaData = new Array();
-var NewData=[] ;
+var DataSearch = new Array();
+var flagHolder = new Array();
 var myThumb;
 
 class TestHomeView extends Component {
+
   constructor(props) {
     super(props);
     var self = this;
@@ -43,11 +45,13 @@ class TestHomeView extends Component {
       selectedItem: undefined,
       search: '',
     };
-    this.loadData(function(m_DATA){});
+    this.loadData(/*function(m_DATA){}*/);
+
+
   }
 
   // load data from Bundle
-  loadData(callback){
+  loadData(/*callback*/){
       RNFS.readDir(RNFS.MainBundlePath + '/mp3files')
       .then((result) => {
       // Load file and push into array DATA
@@ -80,28 +84,30 @@ class TestHomeView extends Component {
       for ( i= 0; i < MetaData.length; i++) {
         //MetaData[i].then(function(metaResult){
         MetaData[i].then((metaResult)=>{
-           this.setNewData(metaResult);
+           this.setNewData(metaResult,flagHolder,MetaData.length);
           });
+//day
       }
-      callback(DATA);
+      // callback(DATA);
 
       // set DATA to items in state
-      this.setState({
-        items:  DATA,
-        isLoading: false
-      });
+      // this.setState({
+      //   items:  DATA,
+      //   isLoading: false
+      // });
     })
     .catch((err) => {
       console.log(err.message, err.code);
       return false;
     });
+
   }
 
   /*
     Function setNewData to DATA
   */
 
-  setNewData=(m_NewData)=> {
+  setNewData=(m_NewData,fH,arraySize)=> {
     // get data to m_NewData
     var {albumName, artist, title, thumb} = m_NewData;
 
@@ -124,6 +130,19 @@ class TestHomeView extends Component {
     DATA[m_NewData.id].artist = artist;
     DATA[m_NewData.id].albumName = albumName;
     DATA[m_NewData.id].thumb = thumb;
+
+    fH.push(true);
+    console.log("arraySize: "+ arraySize);
+    console.log("current fH: "+ fH.length);
+
+    if(fH.length==arraySize){
+      console.log("RenderRenderRenderRenderRenderRenderRenderRenderRender");
+      this.setState({
+          items:  DATA,
+          isLoading: false
+      });
+    }
+
   }
 
   /*
@@ -145,31 +164,49 @@ class TestHomeView extends Component {
     Function Search
   */
   search() {
+    DataSearch=new Array();
+    var searchStatus = false;
     this.setState({
       isLoading: true,
     });
     console.log('search:  ', this.state.search);
     if(this.state.search !== '') {
       for (var i = 0; i < DATA.length; i++) {
+        // var song=this.state.items[i];
+        // var myMeta = song.metaTag;
+        //
+        // song.metaTag.then(function(value){
+        //    myThumb = value.thumb;
+        // });
+        //
+        // console.log("==Thumb==");
+        // console.log(myThumb);
+        // // song.metaObj
         var keySearch = this.state.search.toLowerCase();
-        var keyName = DATA[i].name.toLowerCase();
+        var songName = DATA[i].name.toLowerCase();
+        var albumName = DATA[i].albumName.toLowerCase();
+        var artist = DATA[i].artist.toLowerCase();
+        // console.log('keyName ==== ',DATA[i].name);
 
-        if(keyName.indexOf(keySearch)!==-1) {
-          DataSearch.push(DATA[i]);
-          this.setState({
-            items: DataSearch,
-            isLoading: false
-          });
-        } else {
-          Alert.alert('Error','The song does not exist');
-          this.setState({
-            items:  DATA,
-            isLoading: false
-          });
-          break;
+          if((songName.indexOf(keySearch) !== -1) || (albumName.indexOf(keySearch) !== -1) || (artist.indexOf(keySearch) !== -1 )) {
+            // console.log('=== Tim duoc roi ====',);
+            // console.log( DATA[i].name);
+            DataSearch.push(DATA[i]);
+            this.setState({
+              items: DataSearch,
+              isLoading: false
+            });
+            searchStatus=true;
+          }
         }
-      }
 
+        if(searchStatus==false){
+        Alert.alert('Error','The song does not exist');
+        this.setState({
+          items:  DATA,
+          isLoading: false
+        });
+        }
     } else {
       this.setState({
         items:  DATA,
@@ -180,7 +217,7 @@ class TestHomeView extends Component {
 
 
   render() {
-    return(
+var z = (
       <Container>
         <Header>
           <Title>{this.props.title}</Title>
@@ -257,6 +294,8 @@ class TestHomeView extends Component {
         </Content>
       </Container>
     );
+
+    return z;
   }
 }
 module.exports = TestHomeView;
